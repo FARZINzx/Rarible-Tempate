d8d4ffb5192dac206d19305703471d5b27d71ecf
Farzin Hamzehi <5farzinhamzei@gmail.com>
2023-09-06 01:12:47 +0330

    set up header in sm size and import font awesome icon and font inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'header-background': '#191c1fd9',
-        'logo-background' : '#feda03'
+        "header-background": "#191c1fd9",
+        "logo-background": "#feda03",
+      },
+      backgroundColor : {
+        "text-header-color":
+          "radial-gradient(100% 283.68% at 100% 0%, rgb(252, 201, 20) 0%, rgb(254, 111, 55) 47.95%, rgb(198, 106, 221) 75.34%, rgb(154, 116, 241) 100%)",
       }
     },
   },
   plugins: [],
-}
-
+};
