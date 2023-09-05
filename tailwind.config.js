2e34cd1c3fac2b030febf5f03c81b1dddd2d6a1d
Farzin Hamzehi <5farzinhamzei@gmail.com>
2023-09-05 23:26:08 +0330

    set up header left part

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'header-background': '#191c1fd9',
+        'logo-background' : '#feda03'
+      }
+    },
   },
   plugins: [],
 }
