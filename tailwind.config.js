cd8ac1ad3b67eb8fc8429be366d93b21f533253e
Farzin Hamzehi <5farzinhamzei@gmail.com>
2023-09-06 21:22:28 +0330

    finish header with respponsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,22 @@
 module.exports = {
   content: ["./src/**/*.html"],
   theme: {
-    fontFamily:{
-      'inter': ['Inter', 'sans-serif'],
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
     },
     extend: {
       colors: {
         "header-background": "#191c1fd9",
         "logo-background": "#feda03",
-        'header-nav-link' : '#ffffff99',
+        "header-nav-link": "#ffffff99",
+        'icon-color' : '#575A5B'
       },
-      backgroundColor : {
+      backgroundColor: {
         "text-header-color":
           "radial-gradient(100% 283.68% at 100% 0%, rgb(252, 201, 20) 0%, rgb(254, 111, 55) 47.95%, rgb(198, 106, 221) 75.34%, rgb(154, 116, 241) 100%)",
-          'header-input' : '#ffffff0a',
-      }
+        "header-input": "#ffffff0a",
+        "icon-input": "#333638",
+      },
     },
   },
   plugins: [],
