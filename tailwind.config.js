0909e9f7566721847885fca68f72b2496db8e23f
Farzin Hamzehi <5farzinhamzei@gmail.com>
2023-09-08 12:56:06 +0330

    finish part main section 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,27 +6,31 @@ module.exports = {
       inter: ["Inter", "sans-serif"],
     },
     extend: {
-      width : {
-         '128' : '15.2rem',
-         'dropdown' : '40rem',
+      width: {
+        128: "15.2rem",
+        dropdown: "40rem",
       },
       colors: {
         "header-background": "rgba(25,28,31,.85)",
-        'popUp-menu' : 'rgba(25,28,31,.85)',
+        "popUp-menu": "rgba(25,28,31,.85)",
         "logo-background": "#feda03",
         "header-nav-link": "#ffffff99",
-        'icon-color' : '#575A5B',
-        'span-color' : 'rgba(255,255,255,0.85)',
-        'span-color2' : 'rgba(255,255,255,0.6)',
+        "icon-color": "#575A5B",
+        "span-color": "rgba(255,255,255,0.85)",
+        "span-color2": "rgba(255,255,255,0.6)",
       },
       backgroundColor: {
         "text-header-color":
           "radial-gradient(100% 283.68% at 100% 0%, rgb(252, 201, 20) 0%, rgb(254, 111, 55) 47.95%, rgb(198, 106, 221) 75.34%, rgb(154, 116, 241) 100%)",
         "header-input": "#ffffff0a",
         "icon-input": "#333638",
-        'a-back' : 'rgba(255,255,255,0.04)',
-        'body-background' : '#191C1F',
-        'section-1-background' : "#302F1C"
+        "a-back": "rgba(255,255,255,0.04)",
+        "a-back-hover": "rgba(255,255,255,0.1)",
+        "body-background": "#191C1F",
+        "section-1-background": "#302F1C",
+        "swiper-icon-bg": "#2c3236",
+        "swiper-icon-bg-hover": "#30363B",
+        "onAir-before-bg": "rgba(255,255,255,.2)",
       },
     },
   },
